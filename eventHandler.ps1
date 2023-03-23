4656446f22716399a9487c88c981d2b6670a97fc
lucabindertbz <luca.binder@edu.tbz.ch>
2023-03-23 15:36:09 +0100

    added code example from teacher

---
+++ eventHandler.ps1
@@ -0,0 +1,31 @@
+﻿function eventHandler()
+{
+    Write-Host "Hallo"
+}
+
+function mouseMoveHandler()
+{
+    $msg = "X: {0} Y: {1}" -f $_.X, $_.Y
+    Write-host $msg
+}
+
+function KeyDownHandler()
+{
+    Write-host $_.KeyData
+}
+
+$f = New-Object System.Windows.Forms.Form
+$f.Add_MouseMove({ mouseMoveHandler})
+
+$b = New-Object System.Windows.Forms.Button
+$b.Text = "OK"
+$b.Add_Click({ eventHandler })
+$f.Controls.Add($b)
+
+$tb = New-Object System.Windows.Forms.TextBox
+$f.Controls.Add($tb)
+$tb.Add_KeyDown({ KeyDownHandler})
+
+$f.ShowDialog()
+
+# $f = $null
