b74adadee8f6c8979bd6c0c97d6029ac8ef8a183
lucabindertbz <luca.binder@edu.tbz.ch>
2023-03-23 16:19:20 +0100

    added all scripts to powershell

---
+++ schedule_email.ps1
@@ -0,0 +1,12 @@
+﻿# Define the name and description of the task
+$TaskName = "Send menu Email"
+$TaskDescription = "This task sends an Email with the Ambassador menu every weekday morning at 08:00"
+
+# Define the action to be taken by the task
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-file `"C:\Users\Administrator\send_mail.ps1`""
+
+# Define the trigger for the task
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 8:00am
+
+# Create the scheduled task
+Register-ScheduledTask -TaskName $TaskName -Description $TaskDescription -Action $Action -Trigger $Trigger
---
+++ schedule_open_outlook.ps1
@@ -0,0 +1,12 @@
+﻿# Define the name and description of the task
+$TaskName = "Open Outlook"
+$TaskDescription = "This task opens Outlook every weekday morning at 07:50"
+
+# Define the action to be taken by the task
+$Action = New-ScheduledTaskAction -Execute "runas.exe" -Argument "/savecred /user:Administrator `"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE`""
+
+# Define the trigger for the task
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 7:50am
+
+# Create the scheduled task
+Register-ScheduledTask -TaskName $TaskName -Description $TaskDescription -Action $Action -Trigger $Trigger
