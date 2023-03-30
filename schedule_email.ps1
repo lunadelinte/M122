983682b94e126aa91bebdb3fb7a9e6fd38ff05d1
lucabindertbz <luca.binder@edu.tbz.ch>
2023-03-30 13:10:06 +0200

    finished project

--- schedule_email.ps1
+++ schedule_email.ps1
@@ -3,7 +3,7 @@ $TaskName = "Send menu Email"
 $TaskDescription = "This task sends an Email with the Ambassador menu every weekday morning at 08:00"
 
 # Define the action to be taken by the task
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-file `"C:\Users\Administrator\send_mail.ps1`""
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-file `"$PSScriptRoot\send_mail.ps1`""
 
 # Define the trigger for the task
 $Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At 8:00am
